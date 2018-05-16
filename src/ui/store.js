fe0a0430e1f7744215753249f860d10aeb8873d9
Tsvetilian Yankov <tsvetilian.ty@gmail.com>
2018-05-16 10:14:33 +0300

    add redux middleware (logger and thunk)

--- src/ui/store.js
+++ src/ui/store.js
@@ -1,11 +1,16 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import movies from './reducers/moviesReducer';
+
+const logger = createLogger();
 
 const store = createStore(
     combineReducers({
       movies,
     }),
+    applyMiddleware(logger, thunk),
 );
 
 export default store;
